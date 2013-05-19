9c7f627712242ed31da81b742b1e131ea5d49e5f
Shaun Jones <jonessc6@gmail.com>
2013-05-19 18:39:21 -0500

    Added our first block

---
+++ mods/jonessc2016tuts/common/BlocktutBlock.java
@@ -0,0 +1,11 @@
+package mods.jonessc2016tuts.common;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlocktutBlock extends Block{
+
+	public BlocktutBlock(int par1, Material par2Material) {
+		super(par1, par2Material);
+	}
+}
--- mods/jonessc2016tuts/common/jonessc2016tuts.java
+++ mods/jonessc2016tuts/common/jonessc2016tuts.java
@@ -1,12 +1,20 @@
 package mods.jonessc2016tuts.common;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
 
 
 @Mod(modid="Tut", name="Tut", version="1.5.1 - 1.5.2")
@@ -19,15 +27,22 @@ public class jonessc2016tuts {
 	@SidedProxy(clientSide="mods.jonessc2016tuts.client.clientProxy", serverSide="mods.jonessc2016tuts.common.commonProxy")
 	public static commonProxy proxy;
 	
+	public static Block tutBlock;
+	
+	@PreInit
 	public void pre(FMLPreInitializationEvent event){
 		
 	}
 
-	
+	@Init
 	public void load(FMLInitializationEvent event){
+		tutBlock=new BlocktutBlock(1000, Material.rock).setCreativeTab(CreativeTabs.tabBlock).setUnlocalizedName("Tut Block");
 		
+		GameRegistry.registerBlock(tutBlock, "Tut Block" + tutBlock.getUnlocalizedName2());
+		LanguageRegistry.addName(tutBlock, "Tut Block");
 	}
 	
+	@PostInit
 	public void post(FMLPostInitializationEvent event){
 		
 	}
